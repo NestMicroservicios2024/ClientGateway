3f4426c59425483dbac1f246b44e66cacb798b7f
JaviSrz <ljsuarez7@gmail.com>
2024-09-15 13:08:16 +0200

    prueba submodulo subida

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,9 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter())
   await app.listen(envs.port);
 
+  console.log('Hola mundo - Primer cambio');
+  
+
   logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
